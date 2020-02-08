e85d6e3cb7e73669811097f3023d3a769caa18b0
Dustin <dustinchristians@gmail.com>
2020-02-08 15:55:36 -0500

    Added a simple menu.

--- ps-flux/src/components/App.js
+++ ps-flux/src/components/App.js
@@ -3,12 +3,21 @@
 import React from 'react';
 import HomePage from './HomePage';
 import AboutPage from './AboutPage';
+import Header from './common/Header';
 
 function App() {
-  // window.location is built into all browsers
-  const route = window.location.pathname;
-  if (route === '/about') return <AboutPage />;
-  return <HomePage />;
+  function getPage() {
+    // window.location is built into all browsers
+    const route = window.location.pathname;
+    if (route === '/about') return <AboutPage />;
+    return <HomePage />;
+  }
+  return (
+    <div className='container-fluid'>
+      <Header />
+      {getPage()}
+    </div>
+  );
 }
 
 export default App;
