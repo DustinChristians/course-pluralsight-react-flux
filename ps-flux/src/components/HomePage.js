4a259df55f15aa294aad96de0a286b45a8ccdad3
Dustin <dustinchristians@gmail.com>
2020-02-08 15:10:27 -0500

    Created the HomePage component.

---
+++ ps-flux/src/components/HomePage.js
@@ -0,0 +1,27 @@
+// ES import syntax. Native JavaScript imports were added to the language in 2015.
+// This lines says import an NPM package named 'react' and store it in a variable
+// called 'React'
+import React from 'react';
+
+// Using functional component instead of a class component.
+// Function names should begin with a capital letter which is important for 2 reasons.
+// 1. React components are instantiated and JavaScript classes and functions that create
+//    an instance traditionally start with a capital letter.
+// 2. React assumes that any elements in JSX that start with a capital letter are assumed to be
+//    React components. Lowercase elements are assumed to be native HTML.
+// Function components render whatever JSX we return.
+function HomePage() {
+  return (
+    <div className='jumbotron'>
+      <h1>Pluralsight Administration</h1>
+      <p>React, Flux, and react Router for ultra-responsive web apps.</p>
+    </div>
+  );
+}
+
+// By default, everything in each file is private. That's because create React app is configured to use
+// ES modules and everything in an ES module is private by default. So, we need to export the component
+// so it can be used by other files. Traditionally, if only a single item is being exported, it is declared
+// as the "default" export. The benefit of a default export is that it requires a little less code to
+// import and the file doing the import can decide what to name that import.
+export default HomePage;
--- ps-flux/src/index.js
+++ ps-flux/src/index.js
@@ -1,1 +1,22 @@
+// This is the application's entry point. create-react-app looks at the index.js imports to
+// determine what files are in the application.
+
 import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+// using a named import for react-dom and we will import the render function. Named imports
+// are a handy way to get a reference to a function inside the element that we're importing.
+// So this creates a const called render that references react-dom's render function.
+// This is equivalent to:
+//      import ReactDOM from 'react-dom'
+//      const render = ReactDOM.render;
+// The render function is what will render the application.
+import { render } from 'react-dom';
+// We want to render the HomePage so we'll import that as well
+import HomePage from './components/HomePage';
+
+// Render accepts two arguments, the component we want to render and the DOM element
+// where we want to place our application. To determine where we want to render we can
+// open up index.html where we'll find a div with an id of "root" which is where we'll
+// mount the application.
+// JSX supports self-closing tags like HTML
+render(<HomePage />, document.getElementById('root'));
