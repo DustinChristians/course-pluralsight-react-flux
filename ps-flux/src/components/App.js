c533dc24005b27efd1771a2040c0dd86d3798a3d
Dustin <dustinchristians@gmail.com>
2020-02-26 06:45:32 -0500

    Added a 404 page.

--- ps-flux/src/components/App.js
+++ ps-flux/src/components/App.js
@@ -5,7 +5,8 @@ import HomePage from './HomePage';
 import AboutPage from './AboutPage';
 import Header from './common/Header';
 import CoursesPage from './CoursesPage';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
+import NotFoundPage from './NotFoundPage';
 
 function App() {
   return (
@@ -14,9 +15,13 @@ function App() {
       {/* The "exact" syntax here says that this route should only match
       if the URL is exactly "/". We need to do this or the "/courses" route
       will render the HomePage component and the CoursesPage component. */}
-      <Route path='/' exact component={HomePage} />
-      <Route path='/courses' component={CoursesPage} />
-      <Route path='/about' component={AboutPage} />
+      <Switch>
+        {/* First route found is used because of the Switch */}
+        <Route path='/' exact component={HomePage} />
+        <Route path='/courses' component={CoursesPage} />
+        <Route path='/about' component={AboutPage} />
+        <Route component={NotFoundPage} />
+      </Switch>
     </div>
   );
 }
