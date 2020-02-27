83f12fdc2bc09c4b09c217584f9923a6efd91473
Dustin <dustinchristians@gmail.com>
2020-02-27 05:27:12 -0500

    Added an about page redirect.

--- ps-flux/src/components/App.js
+++ ps-flux/src/components/App.js
@@ -5,7 +5,7 @@ import HomePage from './HomePage';
 import AboutPage from './AboutPage';
 import Header from './common/Header';
 import CoursesPage from './CoursesPage';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import NotFoundPage from './NotFoundPage';
 
 function App() {
@@ -20,6 +20,8 @@ function App() {
         <Route path='/' exact component={HomePage} />
         <Route path='/courses' component={CoursesPage} />
         <Route path='/about' component={AboutPage} />
+        {/* This will redirect the user to the /about page when they request /about-page */}
+        <Redirect from='/about-page' to='/about' />
         <Route component={NotFoundPage} />
       </Switch>
     </div>
