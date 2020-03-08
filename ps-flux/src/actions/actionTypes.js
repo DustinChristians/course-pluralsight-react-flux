505b543f9f8226ec7335b899e0e1debd2261b4d7
Dustin <dustinchristians@gmail.com>
2020-03-08 14:18:32 -0400

    Added the save course action.

---
+++ ps-flux/src/actions/actionTypes.js
@@ -0,0 +1,4 @@
+// This file will export a list of our apps action types By declaring our action types
+export default {
+  CREATE_COURSE: 'CREATE_COURSE'
+};
---
+++ ps-flux/src/actions/courseActions.js
@@ -0,0 +1,17 @@
+import dispatcher from '../appDispatcher';
+import * as courseApi from '../api/courseApi'; // Wildcard import, so you don't have to maintain an ongoing list of imports
+import actionTypes from './actionTypes';
+
+// This entire function is the action creator.
+export function saveCourse(course) {
+  courseApi.saveCourse(course).then(savedCourse => {
+    // use a return here to return the result fo the promise so that the caller
+    // will be notified when the promise resolves
+    return dispatcher.dispatch({
+      // dispatcher tells all the stores that a course was just created
+      // Anything inside this object is called the action
+      actionType: actionTypes.CREATE_COURSE, // This is the only required property
+      course: savedCourse
+    });
+  });
+}
